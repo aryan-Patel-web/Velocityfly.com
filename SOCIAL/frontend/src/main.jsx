5e34f9f2a74e9609f977a8661b92998151dab271
aryan-Patel-web <aryanpatel77462@gmail.com>
2025-10-28 00:51:11 +0530

    Fix auth security

--- SOCIAL/frontend/src/main.jsx
+++ SOCIAL/frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App'
+import App from './app'
 
 // Global error handler for unhandled promise rejections
 window.addEventListener('unhandledrejection', (event) => {
